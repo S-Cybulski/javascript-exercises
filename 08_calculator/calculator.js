1c4ddc73b8236a17a58dd120c0b6881ec066f056
S-Cybulski <szymcyb04@protonmail.com>
2023-09-15 17:03:13 +0100

    finished exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numArray) {
+	let total = 0;
+  numArray.forEach((num) => {total += num;})
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(numArray) {
+  let total = 1;
+  numArray.forEach((num) => {total *= num;})
+  return total;
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+	return Math.pow(num, power);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let total = 1;
+	for (let i = 1; i <= num; i++){
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
