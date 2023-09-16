75855f84d651246a3d404ec4f3189ed4b176cd61
S-Cybulski <szymcyb04@protonmail.com>
2023-09-16 20:23:31 +0100

    finished exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,24 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let forbiddenLetters = [',','!',' ','.']
+    
+    forbiddenLetters.forEach((letter) => {word = word.replaceAll(letter,'')});
 
-};
+    word = word.toLowerCase();
+
+    let leftPointer = 0, rightPointer = word.length - 1, middle = (word.length + 1) / 2;
+
+    for (let i = 0; i <= middle; i++){
+        if (word[leftPointer] !== word[rightPointer]) return false;
+
+        else {
+            leftPointer += 1; 
+            rightPointer -= 1;
+        }
+    }
+
+    return true
+
+}
 
 // Do not edit below this line
 module.exports = palindromes;
