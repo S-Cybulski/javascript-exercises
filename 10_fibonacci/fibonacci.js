e8a728af59589a0e6959b8657589db1595c1751c
S-Cybulski <szymcyb04@protonmail.com>
2023-09-18 16:32:53 +0100

    finished exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
 
+    if (num <= 0){
+        return "OOPS"
+    }
+
+    let fibArray = [];
+
+    for (let i = 0; i < num; i++){
+        if (fibArray.length === 0 || fibArray.length === 1){
+            fibArray.push(1);
+        }
+        else {
+            fibArray.push(fibArray[fibArray.length - 1] + fibArray[fibArray.length - 2]);
+        }
+    }
+    return fibArray[fibArray.length - 1];
 };
 
 // Do not edit below this line
