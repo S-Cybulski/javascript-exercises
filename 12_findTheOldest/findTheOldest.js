dd64d26ea2f732777074f6ed2427e1dcd57915f6
S-Cybulski <szymcyb04@protonmail.com>
2023-09-19 16:52:10 +0100

    Finished exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+ return people.reduce((oldest, currentPerson) => {
+    const oldestAge = getAge(oldest);
+    const currentAge = getAge(currentPerson);
+    return oldestAge < currentAge ? currentPerson : oldest;
+ });
+  
 };
+
+const getAge = function(person){
+  if (!person.yearOfDeath) person.yearOfDeath = new Date().getFullYear;
+
+  return person.yearOfDeath - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
